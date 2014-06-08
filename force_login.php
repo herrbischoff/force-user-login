532655b839af24122afc2acd3f6007dbe18373fa
Marcel <marcel@herrbischoff.com>
2014-06-08 13:33:47 +0200

    Make code conform with PSR-2 coding standards

--- force_login.php
+++ force_login.php
@@ -1,13 +1,13 @@
 <?php
 /*
-	Plugin Name: Force User Login
-	Plugin URI: https://github.com/bubblun/force-user-login
-	Description: A really small plugin that forces a user to login before being able to view any blog content.
-	Version: 1.3.1
-	Author: The Integer Group Development Team / Marcel Bischoff
-	Author URI: http://www.integer.com
+    Plugin Name: Force User Login
+    Plugin URI: https://github.com/bubblun/force-user-login
+    Description: A really small plugin that forces a user to login before being able to view any blog content.
+    Version: 1.3.1
+    Author: The Integer Group Development Team / Marcel Bischoff
+    Author URI: http://www.integer.com
 
-	Copyright 2009 The Integer Group Dev Team  (email : development@integerdenver.com)
+    Copyright 2009 The Integer Group Dev Team  (email : development@integerdenver.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,42 +24,40 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-	add_action( 'template_redirect', 'force_login' );
+    add_action( 'template_redirect', 'force_login' );
 
-	function force_login()
-	{
-		$redirect_to = $_SERVER['REQUEST_URI']; // Change this line to change to where logging in redirects the user, i.e. '/', '/wp-admin', etc.
+    function force_login()
+    {
+        $redirect_to = $_SERVER['REQUEST_URI']; // Change this line to change to where logging in redirects the user, i.e. '/', '/wp-admin', etc.
 
-		if ( ! is_user_logged_in() )
-		{
-			if ( is_feed() )
-			{
-				$credentials = array();
-				$credentials['user_login'] = $_SERVER['PHP_AUTH_USER'];
-				$credentials['user_password'] = $_SERVER['PHP_AUTH_PW'];
+        if ( ! is_user_logged_in() )
+        {
+            if ( is_feed() )
+            {
+                $credentials = array();
+                $credentials['user_login'] = $_SERVER['PHP_AUTH_USER'];
+                $credentials['user_password'] = $_SERVER['PHP_AUTH_PW'];
 
-				$user = wp_signon( $credentials );
+                $user = wp_signon( $credentials );
 
-				if ( is_wp_error( $user ) )
-				{
-					header( 'WWW-Authenticate: Basic realm="' . $_SERVER['SERVER_NAME'] . '"' );
-					header( 'HTTP/1.0 401 Unauthorized' );
-					die();
+                if ( is_wp_error( $user ) )
+                {
+                    header( 'WWW-Authenticate: Basic realm="' . $_SERVER['SERVER_NAME'] . '"' );
+                    header( 'HTTP/1.0 401 Unauthorized' );
+                    die();
 
-				} // if
+                } // if
 
-			} // if
+            } // if
 
-			else
-			{
+            else
+            {
 
-		  		header( 'Location: ' . get_site_url() . '/wp-login.php?redirect_to=' . $redirect_to );
-				die();
+                header( 'Location: ' . get_site_url() . '/wp-login.php?redirect_to=' . $redirect_to );
+                die();
 
-			} // else
+            } // else
 
-	  	} // if
+        } // if
 
-	} // force_login
-
-?>
+    } // force_login
