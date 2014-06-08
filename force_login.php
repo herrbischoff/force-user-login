f195161a8b8569b2e5209eca7e4d2647cddd6270
Marcel <marcel@herrbischoff.com>
2014-06-08 12:22:47 +0200

    Add plugin URL

--- force_login.php
+++ force_login.php
@@ -1,12 +1,12 @@
 <?php
 /*
 	Plugin Name: Force User Login
-	Plugin URI: n/a
+	Plugin URI: https://github.com/bubblun/force-user-login
 	Description: A really small plugin that forces a user to login before being able to view any blog content.
 	Version: 1.3
 	Author: The Integer Group Development Team / Marcel Bischoff
 	Author URI: http://www.integer.com
-	
+
 	Copyright 2009 The Integer Group Dev Team  (email : development@integerdenver.com)
 
     This program is free software; you can redistribute it and/or modify
@@ -22,14 +22,14 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/	
-	
+*/
+
 	add_action( 'template_redirect', 'force_login' );
-	
+
 	function force_login()
 	{
 		$redirect_to = $_SERVER['REQUEST_URI']; // Change this line to change to where logging in redirects the user, i.e. '/', '/wp-admin', etc.
-		
+
 		if ( ! is_user_logged_in() )
 		{
 			if ( is_feed() )
@@ -45,11 +45,11 @@
 					header( 'WWW-Authenticate: Basic realm="' . $_SERVER['SERVER_NAME'] . '"' );
 					header( 'HTTP/1.0 401 Unauthorized' );
 					die();
-					
+
 				} // if
 
 			} // if
-			
+
 			else
 			{
 
